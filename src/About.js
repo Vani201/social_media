48f51ecad6c6b67d1762b91fafeea13ae995b01a
Vani201 <gokilavani202@gmail.com>
2025-02-04 15:46:51 +0530

    Update About.js
    fixed bug

--- src/About.js
+++ src/About.js
@@ -5,7 +5,8 @@ const About = () => {
      <main className='About'>
             <h2>About</h2>
             <p style={{ marginTop: "1rem" }}>This blog app is a project in the Learn React tutorial series.</p>
-        </main>>
+        </main>
+
   )
 }
 
